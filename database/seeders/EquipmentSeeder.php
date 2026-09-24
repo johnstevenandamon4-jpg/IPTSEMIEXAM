877b72a29037d984b0b32fd3757b2d357af3ad21
MDC-Cl-PC12 <aljonevangelista2.com>
2026-09-24 13:29:55 +0800

    commission

--- database/seeders/EquipmentSeeder.php
+++ database/seeders/EquipmentSeeder.php
@@ -10,10 +10,8 @@ class EquipmentSeeder extends Seeder
 {
     public function run(): void
     {
-        // Seed 50 records directly into the equipments table
         Equipment::factory()->count(50)->create();
 
-        // Seed 1 or more records for the second required table
         Laboratories::factory()->count(1)->create();
     }
 }
